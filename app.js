42616d2ff1db33df76e74d32ba7dbff7c5faf33d
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-11-22 07:41:34 +0500

    update: app.js basic setup

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 // Loading Environment Variables.
 require('dotenv').config({ quiet: true });
 
+// Node Imports.
+const path = require('path');
+
 // Lib Imports.
 const express = require('express');
 
 // Initializing Express.
 const app = express();
+
+// Integrating Template Engine (Pug).
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'src', 'views'));
+
+// Exposing the Public Directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Integrating Middlewares.
+app.use(express.json());
+
+// Routes.
 
 // Running the server.
 app.listen(process.env.PORT);
