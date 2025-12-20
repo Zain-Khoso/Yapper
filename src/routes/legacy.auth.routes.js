e823832d0a62b1a2439690d1e478f041b937894a
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-20 15:35:05 +0500

    fix: Legacy code cleanup

--- src/routes/legacy.auth.routes.js
+++ src/routes/legacy.auth.routes.js
@@ -2,43 +2,22 @@
 import { Router } from 'express';
 
 // Local Imports.
-import { postEmailUnique } from '../controllers/legacy.auth.controller.js';
+import controller from '../controllers/legacy.auth.controller.js';
 
 const router = Router();
 
 // Auth Routes.
 
-// POST: Email Uniqueness Check.
-router.post('/email/unique', postEmailUnique);
+// GET: Account Delete.
+router.get('/account/delete', controller.getAccountDelete);
+
+// POST: Change Password Token.
+router.post('/change-password-token', controller.postActionToken);
+
+// POST: Change Password.
+router.post('/change-password/:token', controller.postChangePassword);
+
+// POST: Change Email.
+router.post('/change-email', controller.postChangeEmail);
 
 export default router;
-
-// // POST: Create Account.
-// router.post('/account/create', protectFromAuthenticatedUsers, controller.postCreateAccount);
-
-// // POST: Login.
-// router.post('/account/login', protectFromAuthenticatedUsers, controller.postLogin);
-
-// // GET: Logout.
-// router.get('/account/logout', protectFromUnAuthenticatedUsers, controller.getLogout);
-
-// // GET: Account Delete.
-// router.get('/account/delete', protectFromUnAuthenticatedUsers, controller.getAccountDelete);
-
-// // POST: Change Password Token.
-// router.post('/change-password-token', controller.postActionToken);
-
-// // POST: Change Password.
-// router.post('/change-password/:token', controller.postChangePassword);
-
-// // POST: Change Email.
-// router.post('/change-email', protectFromUnAuthenticatedUsers, controller.postChangeEmail);
-
-// // POST: Change Display Name.
-// router.post(
-//   '/change-displayName',
-//   protectFromUnAuthenticatedUsers,
-//   controller.postChangeDisplayName
-// );
-
-// module.exports = router;
