2237ea3ed898e6b5ac7c031897ad5d0a03db18b6
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-21 10:41:27 +0500

    new: User Picture signing Endpoint

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -4,6 +4,7 @@ import { Router } from 'express';
 // Local Imports.
 import accountRouter from './account.routes.js';
 import authRouter from './auth.routes.js';
+import uploadRouter from './upload.routes.js';
 
 // API Routes.
 const router = Router();
@@ -13,5 +14,8 @@ router.use('/account', accountRouter);
 
 // User Authentication Routes.
 router.use('/auth', authRouter);
+
+// File Upload Routes.
+router.use('/upload', uploadRouter);
 
 export default router;
