d6e68861fb3c18e037afaed5651d67ac2df18155
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-21 20:13:55 +0500

    update: Axios Intercepter attatched to Axios global instance

--- src/client/js/utils.js
+++ src/client/js/utils.js
@@ -34,6 +34,64 @@ const showSuccess = Swal.mixin({
 
 const API = axios.create({ baseURL: '/api/v1' });
 
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error, token = null) => {
+  failedQueue.forEach((prom) => {
+    if (error) prom.reject(error);
+    else prom.resolve(token);
+  });
+
+  failedQueue = [];
+};
+
+API.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401) return location.assign('/login');
+
+    if (error.response?.status === 403 && !originalRequest.__retry__) {
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({ resolve, reject });
+        })
+          .then((token) => {
+            originalRequest.headers['Authorization'] = `Bearer ${token}`;
+            return API(originalRequest);
+          })
+          .catch((err) => Promise.reject(err));
+      }
+
+      originalRequest.__retry__ = true;
+      isRefreshing = true;
+
+      try {
+        const response = await axios.get('/api/v1/auth/refresh', { withCredentials: true });
+        const newToken = response.data.data.accessToken;
+
+        API.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
+        if (!window?.currentUser) window.currentUser = new Map();
+        window.currentUser.set('accessToken', newToken);
+
+        processQueue(null, newToken);
+
+        originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+        return API(originalRequest);
+      } catch (refreshError) {
+        processQueue(refreshError, null);
+        return location.assign('/login');
+      } finally {
+        isRefreshing = false;
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 // Functions.
 function startCooldown(element, seconds = 60) {
   if (!element) return;
