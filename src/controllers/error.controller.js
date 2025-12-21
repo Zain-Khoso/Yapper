f150198a2ddd575b30e1ccb5a8eda7cf6488c644
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-21 20:26:35 +0500

    update: 404 and 500 errors handled separately for API Endpoints.

--- src/controllers/error.controller.js
+++ src/controllers/error.controller.js
@@ -1,10 +1,14 @@
 // Local Imports.
 import { serializeResponse } from '../utils/serializers.js';
 
+async function notFoundError(_, res) {
+  return res.status(404).json(serializeResponse({}, { root: 'This is not a valid Endpoint.' }));
+}
+
 async function serverError(error, _, res, __) {
-  console.log('\n', error, '\n');
+  console.log('\n API server error: ', error, '\n');
 
   return res.status(500).json(serializeResponse({}, { root: 'Something went wrong.' }));
 }
 
-export { serverError };
+export { notFoundError, serverError };
--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -5,6 +5,7 @@ import { Router } from 'express';
 import accountRouter from './account.routes.js';
 import authRouter from './auth.routes.js';
 import uploadRouter from './upload.routes.js';
+import { notFoundError, serverError } from '../controllers/error.controller.js';
 
 // API Routes.
 const router = Router();
@@ -17,5 +18,11 @@ router.use('/auth', authRouter);
 
 // File Upload Routes.
 router.use('/upload', uploadRouter);
+
+// Handles invalid API Endpoints.
+router.use(notFoundError);
+
+// Handles server errors for API routes secifically.
+router.use(serverError);
 
 export default router;
