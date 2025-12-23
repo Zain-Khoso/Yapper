c6f715d347c92824a272527089173609b60927a6
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-23 15:35:17 +0500

    fix: Room UI Loading

--- src/client/js/App.js
+++ src/client/js/App.js
@@ -35,6 +35,66 @@ export default class App {
     this.elem_BtnAddChat.addEventListener('click', () => this.createRoom());
   }
 
+  addRoom(room, mode = 'pagination') {
+    if (this.rooms.has(room.id)) return;
+    else this.rooms.set(room.id, room);
+
+    const {
+      id: roomId,
+      receiver: { initial, picture, displayName, isOnline },
+      lastSpoke,
+      lastMessage,
+      unreadCount,
+    } = room;
+
+    const roomHTML = `
+      <li class="chat${unreadCount === 0 ? '' : ' unread'}" data-roomId=${roomId}>
+        ${
+          picture
+            ? `
+          <div class="avatar image">
+            <img alt="${displayName}'s Image" src="${picture}"  /> 
+            
+            <div class="status">
+              <div class="dot"></div>
+            </div>
+          </div>
+          `
+            : `
+          <div class="avatar fallback"> 
+            <span class="initial"> ${initial} </span>
+            
+            <div class="status">
+              <div class="dot"></div>
+            </div>
+          </div>
+        `
+        }
+        
+        <div class="user-content">
+          <div class="title">
+            <span class="bold"> ${displayName} </span>
+            <span class="last-spoke"> ${lastSpoke} </span>
+          </div>
+          
+          
+          <div class="subtitle">
+            <span class="last-message"> ${lastMessage} </span>
+            <span class="new-message-count"> ${unreadCount > 99 ? '99+' : unreadCount} </span>
+          </div>
+        </div>
+      </li>
+    `;
+
+    if (mode === 'pagination') {
+      if (this.elem_ChatsList.children.length === 1) {
+        this.elem_ChatsList.insertAdjacentHTML('afterbegin', roomHTML);
+      } else {
+        this.elem_ChatsList.children.item(-2).insertAdjacentHTML('afterend', roomHTML);
+      }
+    } else this.elem_ChatsList.insertAdjacentHTML('afterbegin', roomHTML);
+  }
+
   async createRoom() {
     await Swal.fire({
       icon: 'question',
@@ -57,7 +117,7 @@ export default class App {
         const result = schema_Email.safeParse(value);
         if (!result.success) return getZodError(result);
       },
-      preConfirm: async function (receiverEmail) {
+      preConfirm: async (receiverEmail) => {
         try {
           Swal.disableInput();
           Swal.disableButtons();
@@ -74,9 +134,9 @@ export default class App {
             });
           }
 
-          await new showSuccess({ title: 'New Friend Added', timer: 1500 });
+          new showSuccess({ title: 'New Friend Added', timer: 1500 });
 
-          console.log(room);
+          this.addRoom(room, 'new');
         } catch (error) {
           Swal.enableInput();
           Swal.enableButtons();
