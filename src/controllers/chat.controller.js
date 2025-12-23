b9368df6a774019f3d05282528d1dac6657c0f42
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-23 15:37:35 +0500

    fix: Legacy code for creating room removed

--- src/controllers/chat.controller.js
+++ src/controllers/chat.controller.js
@@ -14,7 +14,7 @@ const User = require('../models/user.model');
 const Chatroom = require('../models/chatroom.model');
 const ChatroomMember = require('../models/chatroomMember.model');
 const Message = require('../models/message.model');
-const { schema_email, schema_file } = require('../utils/validations');
+const { schema_file } = require('../utils/validations');
 const { formatChatroom, formatMessage, formatMessagesList } = require('../utils/formatters');
 const storage = require('../utils/storage');
 
@@ -89,102 +89,6 @@ exports.getChatrooms = function (req, res, next) {
     })
     .catch((error) => {
       next(Error(error));
-    });
-};
-
-exports.postAddChatroom = function (req, res) {
-  let { receiverEmail } = req.body;
-  const session = req.session;
-
-  // Sanitizing body data.
-  receiverEmail = validator.trim(receiverEmail);
-  receiverEmail = validator.normalizeEmail(receiverEmail, {
-    gmail_remove_dots: false,
-    gmail_remove_subaddress: false,
-    outlookdotcom_remove_subaddress: false,
-    yahoo_remove_subaddress: false,
-    icloud_remove_subaddress: false,
-  });
-
-  // Validating body data.
-  const result_receiverEmail = schema_email.safeParse(receiverEmail);
-
-  if (!result_receiverEmail.success) {
-    return res.status(409).json({
-      errors: {
-        receiverEmail: result_receiverEmail?.error?.issues?.at(0)?.message,
-      },
-    });
-  }
-
-  if (receiverEmail === session.user.email) {
-    return res.status(409).json({
-      errors: {
-        receiverEmail: 'Invalid Email',
-      },
-    });
-  }
-
-  sequelize
-    .transaction((t) =>
-      User.findOne({ where: { email: receiverEmail }, transaction: t })
-        .then((receiver) => {
-          if (!receiver) throw { errors: { receiverEmail: 'User does not exist.' } };
-
-          return Promise.all([
-            receiver,
-            Chatroom.findAll({
-              attributes: ['id', 'lastMessageAt'],
-              include: [
-                {
-                  model: User,
-                  where: {
-                    id: [receiver.id, session.user.id],
-                  },
-                  attributes: ['id', 'displayName'],
-                  through: { attributes: ['id', 'isBlocked', 'lastReadAt'] },
-                },
-                {
-                  model: Message,
-                  attributes: ['id', 'isFile', 'content', 'createdAt'],
-                  order: [['createdAt', 'DESC']],
-                  limit: 1,
-                  separate: true,
-                },
-              ],
-              transaction: t,
-            }),
-          ]);
-        })
-        .then(([receiver, chatrooms]) => {
-          const existingRoom = chatrooms.find((chatroom) => chatroom.Users.length === 2);
-
-          if (existingRoom) throw { chatroom: formatChatroom(existingRoom, session.user.id) };
-
-          return Promise.all([receiver, Chatroom.create({}, { transaction: t })]);
-        })
-        .then(([receiver, chatroom]) =>
-          chatroom.addUsers([receiver.id, session.user.id], { transaction: t })
-        )
-        .then(([chatroomMember]) =>
-          Chatroom.findByPk(chatroomMember.ChatroomId, {
-            attributes: ['id', 'lastMessageAt'],
-            include: {
-              model: User,
-              attributes: ['id', 'displayName'],
-              through: { attributes: ['id', 'isBlocked', 'lastReadAt'] },
-            },
-            transaction: t,
-          })
-        )
-    )
-    .then((chatroom) =>
-      res.status(200).json({ chatroom: formatChatroom(chatroom, session.user.id) })
-    )
-    .catch((error) => {
-      if (error?.chatroom) res.status(200).json(error);
-      else if (error?.errors) res.status(409).json(error);
-      else res.status(500).json({ errors: { root: 'Something went wrong.' } });
     });
 };
 
