8d30cfdc2e0af1b03ea55d69583814962983a5b5
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-23 15:02:42 +0500

    fix: Model Associations

--- src/utils/associations.js
+++ src/utils/associations.js
@@ -5,32 +5,28 @@ import ChatroomMember from '../models/chatroomMember.model.js';
 import Message from '../models/message.model.js';
 
 // Model Associations.
-User.belongsToMany(Chatroom, { through: ChatroomMember, as: 'member', foreignKey: 'memberId' });
-Chatroom.belongsToMany(User, { through: ChatroomMember, as: 'room', foreignKey: 'roomId' });
+User.belongsToMany(Chatroom, {
+  through: ChatroomMember,
+  as: 'rooms',
+  foreignKey: 'memberId',
+});
+Chatroom.belongsToMany(User, {
+  through: ChatroomMember,
+  as: 'members',
+  foreignKey: 'roomId',
+});
 
-User.hasMany(ChatroomMember, {
-  onUpdate: 'CASCADE',
-  onDelete: 'SET NULL',
-});
-ChatroomMember.belongsTo(User, {
-  onUpdate: 'CASCADE',
-  onDelete: 'SET NULL',
-});
+User.hasMany(ChatroomMember, { foreignKey: 'memberId' });
+ChatroomMember.belongsTo(User, { foreignKey: 'memberId' });
 
-Chatroom.hasMany(ChatroomMember, {
-  onUpdate: 'CASCADE',
-  onDelete: 'CASCADE',
-});
-ChatroomMember.belongsTo(Chatroom, {
-  onUpdate: 'CASCADE',
-  onDelete: 'CASCADE',
-});
+Chatroom.hasMany(ChatroomMember, { foreignKey: 'roomId' });
+ChatroomMember.belongsTo(Chatroom, { foreignKey: 'roomId' });
 
 Chatroom.hasMany(Message, { foreignKey: 'roomId', onDelete: 'CASCADE' });
-Message.belongsTo(Chatroom, { foreignKey: 'roomId', onDelete: 'CASCADE' });
+Message.belongsTo(Chatroom, { foreignKey: 'roomId' });
 
-User.hasMany(Message, { onDelete: 'SET NULL' });
-Message.belongsTo(User, { onDelete: 'SET NULL' });
+User.hasMany(Message, { foreignKey: 'userId', onDelete: 'SET NULL' });
+Message.belongsTo(User, { foreignKey: 'userId', as: 'sender' });
 
 ChatroomMember.hasMany(Message, { foreignKey: 'senderId', onDelete: 'SET NULL' });
 Message.belongsTo(ChatroomMember, { foreignKey: 'senderId', onDelete: 'SET NULL' });
