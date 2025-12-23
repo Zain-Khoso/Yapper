75e87e49f5ccf735cd7c12607b2614157e9c4016
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-23 15:03:12 +0500

    fix: Email Sanitizer

--- src/utils/sanitizers.js
+++ src/utils/sanitizers.js
@@ -3,13 +3,16 @@ import validator from 'validator';
 import he from 'he';
 
 function sanitizeEmail(email) {
-  return validator.normalizeEmail(email.trim(), {
+  email = validator.trim(email);
+  email = validator.normalizeEmail(email.trim(), {
     gmail_remove_dots: false,
     gmail_remove_subaddress: false,
     outlookdotcom_remove_subaddress: false,
     yahoo_remove_subaddress: false,
     icloud_remove_subaddress: false,
   });
+
+  return email;
 }
 
 function sanitizeText(text) {
