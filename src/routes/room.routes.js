4fa90d5c6205cf31d0b00e74206b0f9953ef6d8d
Zain-Khoso <zain.khoso.dev@gmail.com>
2025-12-23 20:14:05 +0500

    fix: Chatroom Unblock Endpoint

--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -2,7 +2,12 @@
 import { Router } from 'express';
 
 // Local Imports.
-import { blockRoom, createChatroom, readChatrooms } from '../controllers/room.controller.js';
+import {
+  blockRoom,
+  createChatroom,
+  readChatrooms,
+  unblockRoom,
+} from '../controllers/room.controller.js';
 import { allowAuthenticatedUserOnly } from '../utils/auth.utils.js';
 
 // Authentication Routes.
@@ -16,5 +21,8 @@ router.get('/get-all/:offset', allowAuthenticatedUserOnly, readChatrooms);
 
 // PATCH : Blocks the other user for the current user.
 router.patch('/block', allowAuthenticatedUserOnly, blockRoom);
+
+// PATCH : Blocks the other user for the current user.
+router.patch('/unblock', allowAuthenticatedUserOnly, unblockRoom);
 
 export default router;
